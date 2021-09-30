805b1762b6bc29907ec2af85054e6bb122c54fc4
MunoLike <sukorice@gmail.com>
2021-09-30 10:44:29 +0900

    add 3-1

---
+++ 3-1/script/script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el:'#app',
+    data(){
+        return {text:'hello Vue.js'};
+    },
+    methods:{
+        handleClick(){
+            this.text='button pushed';
+        }
+    }
+});
